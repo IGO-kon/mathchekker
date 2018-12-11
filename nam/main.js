3f87043eca5b947e22294420373e578981a3789a
IGO(いぐお) <43200655+IGO-kon@users.noreply.github.com>
2018-12-12 00:06:55 +0900

    Add files via upload

---
+++ nam/main.js
@@ -0,0 +1,29 @@
+
+
+var a = 0;
+
+    console.log('a',a);
+
+var b = a/2;
+
+    console.log('b',b);
+
+var c = a*2;
+
+    console.log('c',c);
+
+var d = c/2;
+
+    console.log('d',d);
+
+var e = c/a;
+
+    console.log('e',e); 
+
+var f = a/a;
+
+    console.log('f',f);
+
+var g = 0*2/0;
+
+    console.log('g',g);
